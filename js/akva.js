de55d9887b1fa166b9ef2af718bab40426f9878c
robertpiira <robert@piira.se>
2011-05-09 16:23:39 +0200

    restructring of js

--- js/akva.js
+++ js/akva.js
@@ -6,42 +6,63 @@
 
 
 var akvaGrid =  function() {
+    
     var _this;
     
+    var defaults = {
+    	cols: 12,
+    	leading: 22,
+    	opacity: 5
+    };
+    
     return {
+    
         init: function () {
             _this = this;
+            
+            this.populateValuesFromStorageOrDefaults();
             this.buildGrid();
             this.gridHeight();
-            this.newGridHeight();
-            this.setGrid();
+            
+            $(window).resize(this.gridHeight);
+            
+            $('#akva-form').change(function() {
+            	_this.saveToStorage();
+            	_this.buildGrid();
+            });
           
         },
         
-        buildGrid: function(cols, baselineHeight, opac) { 
+        buildGrid: function() { 
         
+        	var opts = {
+			    	cols: $('#grid-size').val(),
+			    	leading: $('#line-size').val(),
+			    	opacity: $('#akva-opacity').val()
+			};
 		
 			//Columns
 			var colMarkup = "";
-			for (var i = 0, ii = cols; i < ii; i++) {
+			for (var i = 0, ii = opts.cols; i < ii; i++) {
 				colMarkup += '<li><div></div></li>';
 			}
 			
 			//Baseline
-			var baselineHeight = baselineHeight - 1;
-			var lines = $(document).height() / baselineHeight;
+			var leading =  opts.leading - 1;
+			var lines = $(document).height() / leading;
 			var lineMarkup = "";
 			for (var i = 0, ii = lines; i < ii; i++) {
 				lineMarkup += '<li></li>';
 			}
 			
 			//Opacity
-			var opac = opac / 10;
+			var opac = opts.opacity / 10;
 			$('#akva-grid').css('opacity',opac);			
 			
+			//
 			$('.akva-baseline').html(lineMarkup);
 			$('.akva-cols').html(colMarkup);
-			$('.akva-baseline li').css('height',baselineHeight);	
+			$('.akva-baseline li').css('height',leading);	
 		},
         
         gridHeight: function() {
@@ -49,69 +70,41 @@ var akvaGrid =  function() {
 			$('#akva-grid').height(documentHeight);
 		},
 		
-		newGridHeight: function() {
-			$(window).resize(this.gridHeight);
+		saveToStorage: function() {
+		
+			var $form = $('#akva-form');
+			var data = $form.serializeArray();
+		
+			localStorage.setItem("gridData", "nje");
+	
+			$.each(data, function(i, obj) {
+				localStorage.setItem(obj.name, obj.value);
+			});
+			
 		},
 		
-		setGrid: function() {	
-			$('#akva-form').change(function() {
-				
-				var cols = $('#grid-size').val();
-				var baselineHeight = $('#line-size').val();
-				var opac = $('#akva-opacity').val();
-				
-				_this.buildGrid(cols, baselineHeight, opac);
-				
-			});
-		}		
+		populateValuesFromStorageOrDefaults: function() {
+		
+			var $form = $('#akva-form');
+			
+			if (localStorage.getItem("gridData") == "nje") {
+	
+				var data = $form.serializeArray();
+		
+				$.each(data, function(i, obj) {
+					$('[name="' + obj.name + '"]').val(localStorage.getItem(obj.name));
+				});
+						
+			} else {
+			
+				$('#grid-size').val(defaults.cols);
+			    $('#line-size').val(defaults.leading);
+			   	$('#akva-opacity').val(defaults.opacity);
+			
+			}
+		}	
 		
 	}
 }();
 
-
-
-
-
-
 $(window).load(akvaGrid.init());
-
-
-
-
-$(function() {
-
-	var $form = $('#akva-form');	
-	
-	$form.change(function() {
-	
-		var data = $form.serializeArray();
-	
-		localStorage.setItem("gridData", "set");
-
-		$.each(data, function(i, obj) {
-			localStorage.setItem(obj.name, obj.value);
-		});
-		
-	});
-	
-	if (localStorage.getItem("gridData") == "set") {
-
-		var data = $form.serializeArray();
-
-		$.each(data, function(i, obj) {
-			$('[name="' + obj.name + '"]').val(localStorage.getItem(obj.name));
-		});
-		
-		var cols = $('#grid-size').val();
-		var baselineHeight = $('#line-size').val();
-		var opac = $('#akva-opacity').val();
-		
-		akvaGrid.buildGrid(cols, baselineHeight, opac);
-		
-	}
-	
-});
-
-
-
-
